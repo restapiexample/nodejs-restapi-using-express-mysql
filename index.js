0a1b53e18d869f73130bed4f16faf3ee3f9362b4
Douglas S Elias <douglassantoselias@gmail.com>
2017-10-11 19:28:37 -0300

    Change to ES6 syntax

--- index.js
+++ index.js
@@ -1,78 +1,72 @@
-var http = require("http");
-var express = require('express');
-var app = express();
-var mysql      = require('mysql');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const express = require('express');
+const mysql = require('mysql');
+const http = require('http');
+const app = express();
 
-//start mysql connection
-var connection = mysql.createConnection({
-  host     : 'localhost', //mysql database host name
-  user     : 'root', //mysql database user name
-  password : '', //mysql database password
-  database : 'test' //mysql database name
+// MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',     // mysql database host name
+  user: 'root',          // mysql database user name
+  password: 'password',  // mysql database password
+  database: 'test'       // mysql database name
 });
 
-connection.connect(function(err) {
-  if (err) throw err
-  console.log('You are now connected with mysql database...')
+connection.connect(err => {
+  if (err) throw err;
+  console.log('You are now connected with mysql database...');
 })
-//end mysql connection
 
-//start body-parser configuration
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
-//end body-parser configuration
+// body-parser configuration
+app.use(bodyParser.json());                         // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 
-//create app server
-var server = app.listen(3000,  "127.0.0.1", function () {
+// Create server
+const server = app.listen(3000, "127.0.0.1", () => {
+  const host = server.address().address;
+  const port = server.address().port;
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port)
-
+  console.log("Example app listening at http://${host}:${port}");
 });
 
-//rest api to get all customers
-app.get('/customer', function (req, res) {
-   connection.query('select * from customer', function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
-});
-//rest api to get a single customer data
-app.get('/customer/:id', function (req, res) {
-   connection.query('select * from customers where Id=?', [req.params.id], function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
+// Endpoint to get all customers
+app.get('/customer', (req, res) => {
+  connection.query('select * from customer', (error, results, fields) => {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
 });
 
-//rest api to create a new customer record into mysql database
-app.post('/customer', function (req, res) {
-   var params  = req.body;
-   console.log(params);
-   connection.query('INSERT INTO customer SET ?', params, function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
+// Endpoint to get a single customer
+app.get('/customer/:id', (req, res) => {
+  connection.query('SELECT * FROM customer WHERE Id = ?', [req.params.id], (error, results, fields) => {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
 });
 
-//rest api to update record into mysql database
-app.put('/customer', function (req, res) {
-   connection.query('UPDATE `customer` SET `Name`=?,`Address`=?,`Country`=?,`Phone`=? where `Id`=?', [req.body.Name,req.body.Address, req.body.Country, req.body.Phone, req.body.Id], function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
+// Endpoint to create a new customer
+app.post('/customer', (req, res) => {
+  const params = req.body;
+
+  connection.query('INSERT INTO customer SET ?', params, (error, results, fields) => {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
 });
 
-//rest api to delete record from mysql database
-app.delete('/customer', function (req, res) {
-   console.log(req.body);
-   connection.query('DELETE FROM `customer` WHERE `Id`=?', [req.body.Id], function (error, results, fields) {
-	  if (error) throw error;
-	  res.end('Record has been deleted!');
-	});
+// Endpoint to update a customer
+app.put('/customer', (req, res) => {
+  connection.query('UPDATE customer SET Name = ?, Address = ?, Country = ?, Phone = ? WHERE Id = ?', [req.body.Name, req.body.Address, req.body.Country, req.body.Phone, req.body.Id], (error, results, fields) => {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
+});
+
+// Endpoint to delete a customer
+app.delete('/customer', (req, res) => {
+  connection.query('DELETE FROM customer WHERE Id = ?', [req.body.Id], (error, results, fields) => {
+    if (error) throw error;
+    res.end('Record has been deleted!');
+  });
 });
